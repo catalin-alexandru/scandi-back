28d204a05b852210bdf66265ed72d1e62adcccbc
eddie <catalin.alexandru.cosma@gmail.com>
2021-11-10 19:10:34 +0200

    port

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ app.delete("/products/:id", async (req, res) => {
 })
 
 
-app.listen(5000, () => {
-	console.log("server has started on port 5000");
+app.listen(process.env.PORT || 5000, () => {
+	console.log(`server has started on port ${process.env.PORT}`);
 });

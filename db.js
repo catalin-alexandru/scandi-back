074e1b28123184243472655eedfa759e34cb7fec
eddie <catalin.alexandru.cosma@gmail.com>
2021-11-10 19:51:31 +0200

    connection

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-	user: "postgres",
-	password: "2outkast",
-	host: "localhost",
-	port: 5432,
-	database: "scandiweb"
+	 connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
